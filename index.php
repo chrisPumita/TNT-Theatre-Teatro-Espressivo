c7925a32819824a97a8dd989f45cda934d6f204f
Christan RCSG <christian.floppy@gmail.com>
2022-07-02 19:45:57 -0500

    Page 1 Index

---
+++ index.php
@@ -0,0 +1,308 @@
+<!-- ----- VERSION DEL DOCUMENTO ---------
+    VERSION 1.00.0 BUILD 27.06.22
+    @autor: ReCkrea StuDios & limonne
+    @website: reckreastudios.com
+    @webdev: ChrisRCGS & BarbaraB.-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bienvenidos - Espressivo </title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="twitter:description" content="">
+    <meta name="twitter:title" content="">
+    <meta property="og:image" content="assets/img/logo.svg">
+    <meta name="description" content="">
+    <meta property="og:type" content="website">
+    <meta name="twitter:image" content="assets/img/logo.svg">
+    <link rel="icon" type="image/png" sizes="1280x571" href="assets/img/logo.svg">
+    <link rel="icon" type="image/png" sizes="1280x571" href="assets/img/logo.svg">
+    <link rel="icon" type="image/png" sizes="1280x571" href="assets/img/logo.svg">
+    <link rel="icon" type="image/png" sizes="1280x571" href="assets/img/logo.svg">
+    <link rel="icon" type="image/png" sizes="1280x571" href="assets/img/logo.svg">
+    <link rel="stylesheet" href="assets/boostrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700&amp;display=swap">
+    <link rel="stylesheet" href="./assets/vendors/fontawesome-5.15.4/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+    <style>
+        .header {
+            background: url(./assets/img/bg-tnt.jpg) no-repeat center;
+            background-size: cover;
+        }
+
+        .header .navbar {
+            background-color: transparent !important;
+        }
+    </style>
+</head>
+<body>
+
+<!-- Navbar-->
+<header class="header">
+    <nav class="navbar navbar-black navbar-dark bg-dark navbar-expand-lg clean-navbar" style="z-index: 99;">
+        <div class="container">
+            <a href="./"><img src="assets/img/logo.svg" style="width: 250px;" alt="Logo" loading="lazy"></a>
+            <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <span class="navbar-text w-100 d-flex justify-content-sm-start justify-content-md-end">
+                <ul class="navbar-nav ms-5 aos-init aos-animate" data-bss-disabled-mobile="true" data-aos="fade">
+                    <li class="nav-item" >
+                        <a class="nav-link text-light active" data-bss-hover-animate="pulse" href="#" >Home</a>
+                    </li>
+                    <li class="nav-item text-light" data-bss-hover-animate="pulse">
+                        <a class="nav-link text-light" href="#" >TNT Theatre</a>
+                    </li>
+                    <li class="nav-item text-light" data-bss-hover-animate="pulse">
+                        <a class="nav-link text-light" href="#" >Obras</a>
+                    </li>
+                    <li class="nav-item text-light" data-bss-hover-animate="pulse">
+                        <a class="nav-link text-light" href="#" >Actividades</a>
+                    </li>
+                    <li class="nav-item text-light" data-bss-hover-animate="pulse">
+                        <a class="nav-link text-light" href="#" >Blog</a>
+                    </li>
+                    <li class="nav-item text-light" data-bss-hover-animate="pulse">
+                        <button class="btn btn-outline-primary btn-lg" data-bss-hover-animate="pulse" type="submit"><i class="far fa-envelope text-light"></i> Contacto</button>
+                    </li>
+                </ul>
+                </span>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid pb-5">
+        <div class="container">
+            <div class="row" style="margin: 0;">
+                    <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-6 col-xxl-6">
+                        <div class="row">
+                           <div class="border-tittle">
+                               Latin America Tour 2023 - ABRIL Y MAYO
+                           </div>
+                        </div>
+                        <div class="row py-5">
+                            <span class="fst-italic text-light fs-5 py-3"> Teatro en su centro educativo</span>
+                            <img src="./assets/img/header-tittle.svg" alt="">
+                        </div>
+                        <div class="row">
+                            <p class="text-light pe-5 small">
+                                TNT Theatre y Teatro Espressivo le presentan la
+                                gira 2023 Romeo y Julieta / Macbeth. Su centro
+                                educativo podrá disfrutar de estas obras de Shakespeare
+                                en el idioma inglés con talleres y foros
+                                complementarios.
+                            </p>
+                        </div>
+                        <div class="row d-flex align-content-center justify-content-center align-items-center justify-content-center">
+                            <div class="col-sm-6 d-flex justify-content-sm-end py-3">
+                                <button class="btn btn-primary btn-lg w-75 text-black " data-bss-hover-animate="pulse" type="submit">Contáctenos</button>
+                            </div>
+                            <div class="col-sm-6">
+                                <img src="./assets/img/focus.svg" height="60px" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-6 col-xxl-6 position-relative ">
+                        <img class="img-responsive" src="./assets/img/fig-header.png" alt="">
+                        <div class="row position-absolute bottom-0 start-50 translate-middle-x" style="height: 150px; width:100%">
+                            <div class="col-6 col-sm-6 pb-sm-5 ps-sm-5">
+                                <img  class="img-responsive" width="150" src="./assets/img/hoja.svg" alt="">
+                            </div>
+                            <div class="col-6 col-sm-6 pt-sm-5">
+                                <img class="img-responsive" src="./assets/img/cite.svg" alt="">
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</header>
+    <main class="page landing-page">
+        <section class=" bg-grey">
+            <div class="container pt-5">
+                <div class="row">
+                    <div class="col-12 col-sm-5">
+                        <h5 class="text-primary mt-5">TNT Theatre Britain</h5>
+                        <p class="mt-5">
+                            TNT Theatre es la compañía itinerante que <span class="text-primary">más funciones
+                            ha dado de las obras de Shakespeare en todo el
+                            mundo</span>  desde el año 2000. De Beijing a Berlín y de Lima
+                            a Londres, el público y la crítica se han emocionado con
+                            las producciones accesibles y dinámicas que combinan
+                            una narración clara con un teatro físico apasionante.
+                        </p>
+                        <a href="#" class=" text-link">Ver más >></a>
+                        <div class="row py-5">
+                            <div class="card " style="width: 100%; height:350px;">
+                                <div class="card-body bg-secondary p-5">
+                                    <h1 class="card-title text-light" style="font-size: 4rem;"><strong>TOUR <br> LATINO <br> AMÉRICA</strong></h1>
+                                    <h6 class="text-primary">Abril y mayo de 2023</h6>
+                                    <p class="card-text text-light py-5">
+                                        con Teatro Espressivo revive la gira por
+                                        América Latina, llevando las producciones
+                                        más populares, Macbeth y Romeo & Julieta,
+                                        a teatros y lugares educativos desde
+                                        México hasta Chile.
+                                    </p>
+                                    <button class="btn btn-primary btn-lg w-75 text-black" data-bss-hover-animate="pulse" type="submit">Contáctenos</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-7 mt-5 mt-sm-0  d-flex align-content-center justify-content-center  position-relative">
+                        <div class="row">
+                            <img class="" height="500" src="./assets/img/map.svg" alt="Logos eventos">
+                        </div>
+                        <div class="row position-absolute top-50 end-0 translate-middle-y more">
+                            <img class="img-responsive mb-0 mb-sm-5 ps-5" width="80" src="./assets/img/more.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="mt-5">
+            <div class="d-xl-flex d-xxl-flex align-items-xl-center justify-content-xxl-center align-items-xxl-center"
+                 data-bss-parallax-bg="true"
+                 style="height: auto;background: url(./assets/img/px-bg-tabs.jpg) center / cover;">
+                <div class="d-flex d-xxl-flex justify-content-center align-items-center justify-content-xxl-center align-items-xxl-center"
+                     style="width: 100%;height: 100%;">
+                    <!-- Start: 1 Row 1 Column -->
+                    <div class="container">
+                        <div class="row align-items-center mt-5 mb-5">
+                            <div class="col-md-6 position-relative d-flex justify-content-center">
+                            </div>
+                            <div class="col-md-6 align-self-end order-2">
+                                <div style="padding: 2em;margin-top: 2rem; border-radius: 2rem">
+                                    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                        <li class="nav-item tab-my" role="presentation">
+                                            <button class="nav-link tab-button active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+                                        </li>
+                                        <li class="nav-item tab-my" role="presentation">
+                                            <button class="nav-link tab-button" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+                                        </li>
+                                        <li class="nav-item tab-my" role="presentation">
+                                            <button class="nav-link tab-button" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+                                        </li>
+                                    </ul>
+                                    <div class="tab-content border-2 " id="pills-tabContent">
+                                        <div class="tab-pane fade show active text-light" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                                            <h4>Sobre las obras</h4>
+                                            <p>
+                                                "ROMEO & JULIETA" y "MACBETH" son piezas profundas y
+                                                populares de William Shakespeare. Las producciones de Paul
+                                                Stebbings con escenarios históricos estilizados permiten al público
+                                                concentrarse en las obras.
+                                            </p>
+                                            <p>
+                                                La música en vivo y el teatro físico robusto crean una actuación
+                                                apasionante. También hay humor, porque Shakespeare sabía que
+                                                la tragedia es más fuerte si se contrapone con la risa.
+                                            </p>
+                                            <p>
+                                                Ofrecemos dos versiones de actuación a su medida:
+                                            </p>
+                                        </div>
+                                        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+
+                                        </div>
+                                        <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div><!-- End: Parallax Background -->
+        </section>
+    </main>
+<footer class="page-footer bg-grey">
+    <div class="container">
+        <div class="row" style="padding-top: 2em;padding-bottom: 2em;">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 col-xxl-3">
+                <h5>ESPRESSIVO</h5>
+                <p>
+                    El mejor lugar para vivir el arte
+                    en Costa Ricadonde también
+                    podrá degustar la mejor gastronomía
+                    y realizar eventos en el
+                    este de San José.
+                </p>
+            </div>
+            <div class="col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 ps-5">
+                <h5>ENLACES</h5>
+                <div class="row">
+                    <div class="col-5">
+                        <ul class="list-unstyled">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Asesorias técnicas</a></li>
+                            <li><a href="#">Contenido artístico</a></li>
+                            <li><a href="#">Sala VIP reuniones</a></li>
+                            <li><a href="#">Alimentación</a></li>
+                        </ul>
+                    </div>
+                    <div class="col border-start border-dark border-2"></div>
+                    <div class="col-5">
+                        <ul class="list-unstyled">
+                            <li><a href="#">Portafolio</a></li>
+                            <li><a href="#">Mapa</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contacto</a></li>
+                            <li><a href="#">Aviso de privacidad</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 col-xxl-3 text-start ps-3">
+                <h5>CONTACTO</h5>
+                <div class="row">
+                    <div class="col-1">
+                        <h6><i class="fas fa-map-marker-alt me-3"></i></h6>
+                    </div>
+                    <div class="col-11">
+                        <p class="ms-2">Momentum pinares, Teatro espressivo</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-1">
+                        <h6><i class="fas fa-phone-alt"></i></h6>
+                    </div>
+                    <div class="col-11">
+                        <p class="ms-2">(+506) 6360 9158</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-1">
+                        <h6><i class="fas fa-envelope"></i></h6>
+                    </div>
+                    <div class="col-11">
+                        <p class="ms-2"> ventas@espressivo.cr</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright btn-primary" style="padding: 0;margin: 0;">
+        <p>© 2022 espressivo Derechos Reservados</p>
+    </div>
+</footer><!-- End: Footer -->
+</body>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/boostrap/js/bootstrap.min.js"></script>
+<script src="assets/js/script.min.js"></script>
+<script src="./assets/vendors/fontawesome-5.15.4/js/fontawesome.min.js"></script>
+<script src="./assets/vendors/fontawesome-5.15.4/js/all.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenMax.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.js"></script>
+
+</html>
